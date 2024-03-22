cf1758be11a2297faf37c068b157122a73eec334
minestarnl <bramkoene@gmail.com>
2024-03-22 10:54:04 +0100

    Removed docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/bramkoene/Developer/Projects/prosim_offsets/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/bramkoene/Developer/Projects/prosim_offsets/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/bramkoene/Developer/Projects/prosim_offsets/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/bramkoene/Developer/Projects/prosim_offsets/src/css/custom.css"),
-];
